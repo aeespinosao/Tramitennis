93a468b7385f40a527e6d6475fadff801145bb52
TheMachine-PC\Familia <npiedrahitav@unal.edu.co>
2017-04-16 23:56:20 -0500

    ignore

--- application/models/Cancha.php
+++ application/models/Cancha.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Cancha extends CI_Model {
 	public $numero;
 
+
 	public function get_all(){
 		$this->load->database();
 		$query = $this->db->get('cancha');
